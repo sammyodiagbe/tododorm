bed87a15c56f6403158ec878e1626b23979626f9
Odiagbe Samson <sam.odiagbe.dev@gmail.com>
2022-07-23 12:26:57 -0500

    implementing opacity for done task

--- lib/widgets/customTaskTiles.dart
+++ lib/widgets/customTaskTiles.dart
@@ -13,71 +13,75 @@ class CustomClassTile extends StatelessWidget {
   Widget build(BuildContext context) {
     return Consumer<TaskData>(
       builder: (context, provider, child) {
-        return GestureDetector(
-          onLongPress: () {
-            showDialog(
-                context: context,
-                builder: (BuildContext context) {
-                  return AlertDialog(
-                    title: Text('Are you sure you want to delete this task.?'),
-                    actions: [
-                      TextButton(
-                        onPressed: () {
-                          provider.deleteTask(task);
-                          Navigator.pop(context);
-                        },
-                        child: Text(
-                          'Delete',
-                          style: TextStyle(color: redishColor),
+        return Opacity(
+          opacity: task.done ? 0.5 : 1,
+          child: GestureDetector(
+            onLongPress: () {
+              showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return AlertDialog(
+                      title:
+                          Text('Are you sure you want to delete this task.?'),
+                      actions: [
+                        TextButton(
+                          onPressed: () {
+                            provider.deleteTask(task);
+                            Navigator.pop(context);
+                          },
+                          child: Text(
+                            'Delete',
+                            style: TextStyle(color: redishColor),
+                          ),
                         ),
+                        TextButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          child: Text('Go back'),
+                        )
+                      ],
+                    );
+                  });
+            },
+            child: Container(
+              decoration: BoxDecoration(
+                color: tileContainerColor,
+                borderRadius: BorderRadius.all(
+                  Radius.circular(8),
+                ),
+              ),
+              padding: EdgeInsets.all(15),
+              margin: EdgeInsets.symmetric(
+                vertical: 8,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        task.title,
+                        style: titleTextStyle,
                       ),
-                      TextButton(
-                        onPressed: () {
-                          Navigator.pop(context);
-                        },
-                        child: Text('Go back'),
+                      SizedBox(height: 5),
+                      Text(
+                        '10 minutes ago',
+                      ),
+                      SizedBox(height: 10),
+                      CustomTagIndicator(
+                        tag: task.tag,
+                        color: int.parse(task.colorCode),
                       )
                     ],
-                  );
-                });
-          },
-          child: Container(
-            decoration: BoxDecoration(
-              color: tileContainerColor,
-              borderRadius: BorderRadius.all(
-                Radius.circular(8),
+                  ),
+
+                  // toggle button
+
+                  CustomRadioButton(task: task),
+                ],
               ),
-            ),
-            padding: EdgeInsets.all(15),
-            margin: EdgeInsets.symmetric(
-              vertical: 8,
-            ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      task.title,
-                      style: titleTextStyle,
-                    ),
-                    SizedBox(height: 5),
-                    Text(
-                      '10 minutes ago',
-                    ),
-                    SizedBox(height: 10),
-                    CustomTagIndicator(
-                      tag: task.tag,
-                      color: int.parse(task.colorCode),
-                    )
-                  ],
-                ),
-
-                // toggle button
-
-                CustomRadioButton(task: task),
-              ],
             ),
           ),
         );
@@ -138,7 +142,7 @@ class CustomRadioButton extends StatelessWidget {
           width: _childWidth,
           decoration: BoxDecoration(
             shape: BoxShape.circle,
-            color: task.done ? Color(0xff6364FF) : null,
+            color: task.done ? purpish : null,
           ),
         ),
       ),
